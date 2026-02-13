8826fe030751f66ef349d69a285ac51d51715f61
ee268 <sh33dhl@qq.com>
2026-02-14 01:12:23 +0800

    Site updated: 2026-02-14 01:12:22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1771002581947";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1771002724207";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
