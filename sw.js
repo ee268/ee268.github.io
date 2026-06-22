57d650e0d2b7751027c94cd6700888474106ca25
ee268 <sh33dhl@qq.com>
2026-06-22 17:54:00 +0800

    Site updated: 2026-06-22 17:53:59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1780508924681";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1782121771058";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
